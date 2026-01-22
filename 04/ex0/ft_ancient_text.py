449d6969e784669745c16d40735d0b4a67335853
Girard Louis <lgirard@student.42lyon.fr>
2026-01-22 14:17:23 +0100

    04/ex0: realine -> read

--- 04/ex0/ft_ancient_text.py
+++ 04/ex0/ft_ancient_text.py
@@ -1,16 +1,4 @@
 #!/usr/bin/env python3
-from collections.abc import Generator
-from typing import BinaryIO
-
-
-def get_next_line(file: BinaryIO) -> Generator[int, str, None]:
-    line = 1
-    while line:
-        line = file.readline()
-        if line == "" or line is None:
-            return None
-        yield line
-
 
 def ft_ancient_text():
     file_name = "ancient_fragment.txt"
@@ -21,10 +9,10 @@ def ft_ancient_text():
         with open(file_name, 'r') as file:
             print("Connection established...\n")
             print("RECOVERED DATA:")
-            for line in get_next_line(file):
-                print(line, end='')
+            content = file.read()
+            print(content)
             file.close()
-        print("\n\nData recovery complete.", end=" ")
+        print("\nData recovery complete.", end=" ")
     except (FileNotFoundError, PermissionError):
         print("ERROR: Storage vault not found. Run data generator first.\n")
     finally:
