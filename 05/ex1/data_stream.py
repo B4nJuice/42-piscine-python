18e7b73a3daf8fad73ca2726bde48e3b379c98e0
Girard Louis <lgirard@student.42lyon.fr>
2026-01-28 09:18:22 +0100

    05/ex1: TransactionStream process_batch

--- 05/ex1/data_stream.py
+++ 05/ex1/data_stream.py
@@ -65,6 +65,35 @@ class SensorStream(DataStream):
  avg temp: {avg_temp:.1f}°C"
 
 
+class TransactionStream(DataStream):
+    def __init__(self, stream_id: str, data_type: str) -> None:
+        super().__init__(stream_id, "Transaction Stream", data_type)
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        net_flow: int = 0
+
+        for data in data_batch:
+            try:
+                if isinstance(data, str):
+                    key, value = data.split(":")
+                    if key not in ["buy", "sell"]:
+                        raise ValueError(
+                            f"key {key} in {data} is not supported"
+                            )
+
+                    if key == "buy":
+                        net_flow += int(value)
+                    elif key == "sell":
+                        net_flow -= int(value)
+                else:
+                    raise ValueError(f"{data} has to be str.")
+            except Exception:
+                print(f"format error: {data}")
+
+        return f"Trensaction Analysis: {len(data_batch)} operations,\
+ net flow: {net_flow:+} units"
+
+
 def data_stream() -> None:
     print("=== CODE NEXUS - POLYMORPHIC STREAM SYSTEM ===\n")
 
@@ -79,6 +108,23 @@ Type: {sensor_stats.get('data_type')}")
 
     sensor_analysis: str = sensor.process_batch(sensor_batch)
     print(sensor_analysis)
+
+    print()
+
+    transaction: TransactionStream = TransactionStream(
+        "TRANS_001",
+        "Financial Data")
+    transaction_stats: Dict[
+        str, Union[str, int, float]] = transaction.get_stats()
+
+    print(f"Stream ID: {transaction_stats.get('stream_id')},\
+Type: {transaction_stats.get('data_type')}")
+
+    transaction_batch: List[str] = ["buy:100", "sell:150", "buy:75"]
+    print(f"Processing transaction batch: {transaction_batch}")
+
+    transaction_analysis: str = transaction.process_batch(transaction_batch)
+    print(transaction_analysis)
 
 
 if __name__ == "__main__":
