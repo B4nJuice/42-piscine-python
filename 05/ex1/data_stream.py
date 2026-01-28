3cc894c074d11f2e03c54aecbb005da4dafb123a
Girard Louis <lgirard@student.42lyon.fr>
2026-01-28 08:40:34 +0100

    05/ex1: SensorStream process_batch

--- 05/ex1/data_stream.py
+++ 05/ex1/data_stream.py
@@ -6,6 +6,9 @@ from abc import ABC, abstractmethod
 
 class DataStream(ABC):
 
+    def __init__(self, stream_id: int) -> None:
+        self.__stream_id: int = stream_id
+
     @abstractmethod
     def process_batch(self, data_batch: List[Any]) -> str:
         pass
@@ -18,3 +21,33 @@ class DataStream(ABC):
 
     def get_stats(self) -> Dict[str, Union[str, int, float]]:
         pass
+
+
+class SensorStream(DataStream):
+    def process_batch(self, data_batch: List[Any]) -> str:
+        temps: List[float] = []
+        reading_processed = 0
+
+        for data in data_batch:
+            try:
+                if isinstance(data, str):
+                    key, value = data.split(":")
+                    if key not in ["temp", "humidity", "pressure"]:
+                        raise ValueError(
+                            f"key {key} in {data} is not supported"
+                            )
+                    temps.append(float(value))
+                else:
+                    raise ValueError(f"{data} has to be str.")
+                reading_processed += 1
+            except Exception:
+                print(f"format error: {data}")
+
+        n_temp: int = len(temps)
+        if n_temp == 0:
+            avg_temp: str = "undefined"
+        else:
+            avg_temp: float = sum(temps) / len(temps)
+
+        return f"Sensor Analysis: {reading_processed} readings processed,\
+avg temp: {avg_temp:.1f}°C"
