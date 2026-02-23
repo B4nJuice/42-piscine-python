3251a7947693c22ae9a50487f86bdae06f4f05f8
B4nJuice <louis07girard@gmail.com>
2026-02-23 20:35:48 +0100

    07/ex3: Feat: FantasyCardFactory

---
+++ 07/ex3/FantasyCardFactory.py
@@ -0,0 +1,170 @@
+from ex3 import CardFactory
+from ex0 import Card, CreatureCard, Rarity
+from ex1 import (
+                    ArtifactCard,
+                    SpellCard,
+                    SpellEffectType,
+                    ArtifactEffectType,
+                    NumType
+                 )
+import random
+
+
+class FantasyCardFactory(CardFactory):
+    def create_creature(self, name_or_power: str | int | None = None) -> Card:
+        if isinstance(name_or_power, str):
+            name_or_power = name_or_power.lower()
+            match name_or_power:
+                case "dragon":
+                    return CreatureCard(
+                            "Fire Dragon",
+                            5,
+                            Rarity.RARE.value,
+                            3,
+                            8
+                        )
+
+                case "goblin":
+                    return CreatureCard(
+                            "Goblin Warrior",
+                            5,
+                            Rarity.RARE.value,
+                            5,
+                            5
+                        )
+
+                case "minautor":
+                    return CreatureCard(
+                            "Minautor",
+                            7,
+                            Rarity.LEGENDARY.value,
+                            5,
+                            10
+                        )
+
+                case _:
+                    return CreatureCard(
+                            "Fire spirit",
+                            2,
+                            Rarity.COMMON.value,
+                            1,
+                            3
+                        )
+        elif isinstance(name_or_power, int):
+            return CreatureCard(
+                            "Elemental spirit",
+                            name_or_power,
+                            Rarity.UNCOMMON.value,
+                            2,
+                            name_or_power
+                        )
+
+    def create_spell(self, name_or_power: str | int | None = None) -> Card:
+        if isinstance(name_or_power, str):
+            name_or_power = name_or_power.lower()
+
+            match name_or_power:
+                case "fireball":
+                    return SpellCard(
+                        "Fireball",
+                        3,
+                        Rarity.COMMON.value,
+                        SpellEffectType.DAMAGE.value,
+                        3,
+                        {"num_type": NumType.MAX, "num": 3}
+                    )
+
+                case "poison":
+                    return SpellCard(
+                        "Poison Malediction",
+                        7,
+                        Rarity.LEGENDARY.value,
+                        SpellEffectType.DAMAGE.value,
+                        3,
+                        {"num_type": NumType.MIN, "num": 1}
+                    )
+
+                case _:
+                    return SpellCard(
+                        "Angel Dust",
+                        3,
+                        Rarity.UNCOMMON.value,
+                        SpellEffectType.BUFF.value,
+                        5,
+                        {"num_type": NumType.EXACT, "num": 1}
+                    )
+
+        elif isinstance(name_or_power, int):
+            return SpellCard(
+                        "Potion of Healing",
+                        name_or_power,
+                        Rarity.COMMON.value,
+                        SpellEffectType.DAMAGE.value,
+                        name_or_power,
+                        {"num_type": NumType.MAX, "num": 3}
+                    )
+
+    def create_artifact(self, name_or_power: str | int | None = None) -> Card:
+        return ArtifactCard(
+            "Mana Crystal",
+            3,
+            Rarity.COMMON,
+            ArtifactEffectType.MANA.value,
+            2,
+            3
+        )
+
+    def create_themed_deck(self, size: int) -> dict:
+        creatures: dict[str, callable | list[str]] = {
+            "function": self.create_creature,
+            "types":
+                ["dragon", "goblin", "minautor", "fire_spirit"]
+            }
+        spells: dict[str, callable | list[str]] = {
+            "function": self.create_spell,
+            "types":
+                ["fireball", "poison"]
+            }
+        artifacts: dict[str, callable | list[str]] = {
+            "function": self.create_artifact,
+            "types":
+                ["mana_crystal"]
+            }
+
+        deck: list[Card] = []
+
+        for _ in range(size):
+            card_type: dict[str, callable | list[str]] = (
+                random.choice([creatures, spells, artifacts])
+            )
+
+            deck.append(
+                card_type.get("function")(
+                    random.choice(card_type.get("types"))
+                )
+            )
+
+        return {"deck": deck}
+
+    def get_supported_types(self) -> dict:
+        creatures: list[str] = [
+                    "dragon",
+                    "goblin",
+                    "minautor",
+                    "fire_spirit",
+                    "elemental_spirit"
+                ]
+        spells: list[str] = [
+                    "fireball",
+                    "poison",
+                    "angel_dust",
+                    "potion_of_healing"
+                ]
+        artifacts: list[str] = [
+                    "mana_crystal"
+                ]
+
+        supported_types: dict[str, list[str]] = {}
+        supported_types.update({"creatures": creatures})
+        supported_types.update({"spells": spells})
+        supported_types.update({"artifacts": artifacts})
--- 07/ex3/GameEngine.py
+++ 07/ex3/GameEngine.py
@@ -3,17 +3,29 @@ from ex3 import CardFactory, GameStrategy
 
 class GameEngine():
     def __init__(self):
-        pass
+
+        self.turns_simulated: int = 0
+        self.total_damage: int = 0
+        self.cards_created: int = 0
 
     def configure_engine(
                 self,
                 factory: CardFactory,
                 strategy: GameStrategy
             ) -> None:
-        pass
+
+        self.factory: CardFactory = factory
+        self.strategy: GameStrategy = strategy
 
     def simulate_turn(self) -> dict:
         pass
 
     def get_engine_status(self) -> dict:
-        pass
+        engine_status: dict[str, int | str] = {}
+
+        engine_status.update({"turns_simulated": self.turns_simulated})
+        engine_status.update({"strategy_used": self.strategy.name})
+        engine_status.update({"total_damage": self.total_damage})
+        engine_status.update({"card_created": self.cards_created})
+
+        return engine_status
