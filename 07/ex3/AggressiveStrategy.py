8fe789ff6eaed8d507ba66e54d75e87548390f5a
Girard Louis <lgirard@student.42lyon.fr>
2026-02-24 09:48:20 +0100

    07/ex3: Finished: AggressiveStrategy

--- 07/ex3/AggressiveStrategy.py
+++ 07/ex3/AggressiveStrategy.py
@@ -1,5 +1,6 @@
 from ex3 import GameStrategy
 from ex0 import Card, CreatureCard
+from ex1 import SpellCard, SpellEffectType, NumType
 
 
 class AggressiveStrategy(GameStrategy):
@@ -28,3 +29,50 @@ class AggressiveStrategy(GameStrategy):
 
                 hand.remove(card)
                 battlefield.append(card)
+
+        for card in battlefield:
+            if isinstance(card, CreatureCard):
+                actions["damage_dealt"] += card.attack
+                actions["targets_attacked"].append(
+                        self.prioritize_targets(["Enemy Player"])
+                    )
+
+            elif isinstance(card, SpellCard):
+                if SpellCard.effect_type == SpellEffectType.DAMAGE.value:
+                    match SpellCard.num_target.get("num_type"):
+                        case NumType.EXACT:
+                            if SpellCard.num_target.get("num_type") == 1:
+                                actions["damage_dealt"] += card.effect_power
+
+                        case NumType.MIN:
+                            if SpellCard.num_target.get("num_type") <= 1:
+                                actions["damage_dealt"] += card.effect_power
+
+                        case NumType.MAX:
+                            if SpellCard.num_target.get("num_type") >= 1:
+                                actions["damage_dealt"] += card.effect_power
+
+                        case _:
+                            continue
+                    actions["targets_attacked"].append(
+                            self.prioritize_targets(["Enemy Player"])
+                        )
+
+    def get_strategy_name(self) -> str:
+        return "AggressiveStrategy"
+
+    def prioritize_targets(self, available_targets: list) -> list:
+        available_targets = [
+                x for x in available_targets if (
+                        isinstance(x, CreatureCard) or isinstance(x, str)
+                    )
+            ]
+
+        available_targets.sort(
+                key=lambda x: (
+                    0 if isinstance(x, CreatureCard)
+                    else 1, getattr(x, "health", float("inf"))
+                )
+        )
+
+        return available_targets
