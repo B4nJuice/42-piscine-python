32c5b3155e00a48b2f94e3e94816faccca6d7d9f
Girard Louis <lgirard@student.42lyon.fr>
2026-02-24 10:41:21 +0100

    07/ex4: Init

---
+++ 07/ex4/__init__.py
@@ -0,0 +1,12 @@
+from ex4.TournamentCard import TournamentCard
+from ex4.TournamentPlatform import TournamentPlatform
+from ex4.Rankable import Rankable
+
+
+__version__ = "1.0.0"
+__author__ = "lgirard"
+__all__ = [
+        "TournamentCard",
+        "TournamentPlatform",
+        "Rankable"
+    ]
