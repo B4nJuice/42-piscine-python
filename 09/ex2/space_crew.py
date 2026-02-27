94155e554aeff4511a920231082e99572405ccfd
Girard Louis <lgirard@student.42lyon.fr>
2026-02-27 14:58:42 +0100

    save

--- 09/ex2/space_crew.py
+++ 09/ex2/space_crew.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
 class Rank(Enum):
@@ -20,6 +20,9 @@ class CrewMember(BaseModel):
     years_experience: int = Field(..., ge=0, le=50)
     is_active: bool = True
 
+    def __str__(self) -> str:
+        return f"{self.name} ({self.rank.value}) - {self.specialization}"
+
 
 class SpaceMission(BaseModel):
     mission_id: str = Field(..., min_length=5, max_length=15)
@@ -30,3 +33,98 @@ class SpaceMission(BaseModel):
     crew: list[CrewMember] = Field(..., min_length=1, max_length=12)
     mission_status: str = "planned"
     budget_millions: float = Field(..., ge=1, le=10000)
+
+    def __str__(self) -> str:
+        infos: list[str] = [
+                f"Mission: {self.mission_name}",
+                f"ID: {self.mission_id}",
+                f"Destination: {self.destination}",
+                f"Duration: {self.duration_day}{'s'*(self.duration_day > 1)}",
+                f"Budget: ${self.budget_millions:.1f}M",
+                f"Crew size: {len(self.crew)}",
+                "Crew members:"
+        ]
+
+        crew_infos: list[str] = [
+                "- " + member.__str__() for member in self.crew
+            ]
+
+        infos += crew_infos
+
+        return "\n".join(infos)
+
+    @model_validator(mode="after")
+    def mission_validator(self) -> 'SpaceMission':
+        if not self.mission_id.startswith("M"):
+            raise ValueError("Mission ID must start with \"M\"")
+
+        have_commander_or_captain: bool = False
+
+        for member in self.crew:
+            if not member.is_active:
+                raise ValueError("All crew members must be active")
+            match member.rank:
+                case Rank.CAPTAIN | Rank.COMMANDER:
+                    have_commander_or_captain = True
+
+        if not have_commander_or_captain:
+            raise ValueError("Must have at least one Commander or Captain")
+
+        if self.duration_day > 365:
+            experienced_member: int = len(
+                    [member for member in self.crew
+                        if member.years_experience >= 5]
+                )
+            if experienced_member / len(self.crew) < 0.5:
+                raise ValueError(
+                    "Long missions\
+ (> 365 days) need 50% experienced crew (5+ years)")
+
+        return self
+
+
+def main() -> None:
+    sarah: CrewMember = CrewMember(
+            member_id="sarah",
+            name="Sarah Connor",
+            age=45,
+            rank=Rank.COMMANDER,
+            specialization="Mission Command",
+            years_experience=10
+        )
+
+    john: CrewMember = CrewMember(
+            member_id="john",
+            name="John Smith",
+            age=45,
+            rank=Rank.LIEUTENANT,
+            specialization="Navigation",
+            years_experience=5
+        )
+
+    alice: CrewMember = CrewMember(
+            member_id="alice",
+            name="Alice Johnson",
+            age=45,
+            rank=Rank.OFFICER,
+            specialization="Engineering",
+            years_experience=6
+        )
+
+    mars_crew: list[CrewMember] = [sarah, john, alice]
+
+    mars_mission: SpaceMission = SpaceMission(
+        mission_id="M2024_MARS",
+        mission_name="Mars Colony Establishment",
+        destination="Mars",
+        launch_date=datetime.now(),
+        duration_day=900,
+        crew=mars_crew,
+        budget_millions=2500
+    )
+
+    print(mars_mission)
+
+
+if __name__ == "__main__":
+    main()
