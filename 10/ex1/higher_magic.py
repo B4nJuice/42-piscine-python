cf51d187f92295500970273a320fbc028ffbbf3b
Girard Louis <lgirard@student.42lyon.fr>
2026-03-02 11:58:18 +0100

    10/ex1: finished

--- 10/ex1/higher_magic.py
+++ 10/ex1/higher_magic.py
@@ -6,14 +6,14 @@ def power_amplifier(base_spell: callable, multiplier: int) -> callable:
     return lambda: base_spell() * multiplier
 
 
-def conditional_caster(condition: bool, spell: callable) -> callable | str:
-    if condition:
+def conditional_caster(condition: bool, spell: callable) -> callable:
+    if condition():
         return lambda: spell()
     return "Spell fizzled"
 
 
 def spell_sequence(spells: list[callable]) -> callable:
-    return lambda: [spell(i) for i, spell in enumerate(spells)]
+    return lambda: [spell() for spell in spells]
 
 
 def fireball() -> int:
@@ -24,5 +24,37 @@ def heal_potion() -> str:
     return "Heal potion used"
 
 
+def print_42() -> int:
+    print("42")
+    return 42
+
+
+def print_heal_potion() -> str:
+    print(heal_potion())
+    return heal_potion()
+
+
 if __name__ == "__main__":
     combined_spell: callable = spell_combiner(fireball, heal_potion)
+
+    print("Testing spell combiner...")
+    print(f"Combined spell result: {combined_spell()}")
+
+    print()
+
+    print("Testing power amplifier...")
+    print(f"Original: {fireball()},\
+ Amplified: {power_amplifier(fireball, 3)()}")
+
+    print()
+
+    print("Testing conditional caster...")
+    print(f"True: {conditional_caster(lambda: True, heal_potion)()}")
+    print(f"False: {conditional_caster(lambda: False, heal_potion)}")
+
+    print()
+
+    spells: list[callable] = [print_42, print_heal_potion]
+
+    print("Testing spell sequence...")
+    print(spell_sequence(spells)())
